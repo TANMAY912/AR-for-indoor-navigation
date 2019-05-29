e8a1eb86057413534f657b39db3950fa688b27fc
avanish269 <avanish269@gmail.com>
2019-05-30 03:15:17 +0530

    QR Code Scanner Added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AR-for-indoor-navigation
 This repository consists of webpage that uses web AR for convenient indoor navigation.
+A simple QR code scanner using Instascan.
+To be used in indoor navigation web app using Augmented Reality.
+
+To see camera name
+>python -m http.server 8000
+
+Use this command to start local server in the directory. The command should be executed in the directory which contains the files to be hosted.
+
+Then go to localhost:8000/qrcodescanner/src/views. Then after allowing camera access reload the page.
---
+++ qrcodescanner/src/js/qrscanner.js
@@ -0,0 +1,91 @@
+var app=new Vue({
+  el:"#app",
+  data:{
+    scanner:null,
+    activeCameraId:null,
+    cameras:[],
+    scans:[]
+  },
+  mounted:function(){
+    var self=this;
+    var isMobile={
+      Android: function(){
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function(){
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function(){
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function(){
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
+    };
+    var qrcontent="q";
+    var x=123;
+    var count=0;
+    self.scanner=new Instascan.Scanner({video:document.getElementById("pre"),scanPeriod:1});
+    self.scanner.addListener('scan',function(content,image){
+      self.scans.unshift({date:+(Date.now()), content:content});
+      qrcontent=content;
+      x=parseInt(qrcontent);
+      if(isNaN(x)){
+        alert("Invalid Data");
+      }
+      else{
+        x1=x%100;
+        y=Math.floor(x/100);
+        x2=y%100;
+        y=Math.floor(y/100);
+        x3=y%100;
+        y=Math.floor(y/100);
+        x4=y%100;
+        if((x1>=0&&x1<=59)&&(x2>=0&&x2<=59)&&(x3>=0&&x3<=59)&&(x4>=0&&x4<=59)){
+          alert(x1);
+          alert(x2);
+          alert(x3);
+          alert(x4);
+          alert("Valid Data");
+          self.scanner.stop();
+          location.href="https://www.google.com";
+        }
+        else{
+          alert("Invalid Data! Scan another qr code");
+        }
+      }
+    });
+    Instascan.Camera.getCameras().then(function(cameras){
+      self.cameras=cameras;
+      if(cameras.length>0){
+        var i=0;
+        if(isMobile.Android() || isMobile.iOS()){
+          i=1;
+          self.scanner.mirror=false;
+        }
+        self.activeCameraId=cameras[i].id;
+        self.scanner.start(cameras[i]);
+      }
+      else{
+        console.error('No cameras');
+      }
+    }).catch(function(e){
+      console.error(e);
+    });
+  },
+  methods:{
+    formatName:function(name){
+      return name || 'unknown';
+    },
+    selectCamera:function(camera){
+      this.activeCameraId=camera.id;
+      this.scanner.start(camera);
+    }
+  }
+});
